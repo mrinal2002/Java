08f761b60534743a660ad10dc704b6e5448768d5
Mrinal Patra <96857582+mrinal2002@users.noreply.github.com>
2023-08-25 21:54:33 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String arg[]){
+        byte b=127;
+        // byte=128 ; it's showing me an error becouse the range of the bytes is -127 to +127 so it stored the value in between the value.
+        short s=300;
+        int i=2346;
+        float f=3.45f;
+        char c='A';
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+
+        System.out.println(f);
+
+        System.out.println(c);
+
+    }
+}
