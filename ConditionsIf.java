dcac1b3c83c4fa07d45acd8eaae929342d2b1e0f
Mrinal Patra <96857582+mrinal2002@users.noreply.github.com>
2023-09-03 18:04:05 +0530

    Add files via upload

---
+++ ConditionsIf.java
@@ -0,0 +1,11 @@
+public class ConditionsIf {
+    public static void main(String args[]){
+        int x=-5;
+        if(x>=0){
+            System.out.println("positive");
+        }
+        else{
+            System.out.println("negative");
+        }
+    }
+}
